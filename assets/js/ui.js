468896d550da2118e710781d57ac0c7fe5f7b979
awjin <awjinahn@gmail.com>
2017-10-06 13:15:43 -0400

    Add animation to header clicks

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,3 +1,12 @@
-window.onload = function() {
-    document.body.removeAttribute('class');
-};
+window.onload = function () {
+    document.body.removeAttribute('class')
+
+    $('header a').click(function () {
+        var section = $(this).attr('href')
+
+        $('html, body').animate(
+            { scrollTop: $(section).offset().top },
+            200
+        )
+    })
+}
