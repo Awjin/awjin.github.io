fb1bc1d0139abdaed5e3472cce81d0c6765b1549
awjin <awjinahn@gmail.com>
2017-12-05 00:46:32 -0800

    First draft projects section

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,12 +1,3 @@
 window.onload = function () {
     document.body.removeAttribute('class')
-
-    $('header a').click(function () {
-        var section = $(this).attr('href')
-
-        $('html, body').animate(
-            { scrollTop: $(section).offset().top },
-            200
-        )
-    })
 }
